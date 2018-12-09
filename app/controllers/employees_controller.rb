f49e341370f0430904cf33769c5839e212dff0fa
Daniel Hidalgo G <daniel.hg94@gmail.com>
2018-12-09 14:06:37 -0300

    show area and employees number on index-companies

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -10,6 +10,12 @@ class EmployeesController < ApplicationController
     redirect_to @employee.company
   end
 
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to @employee.company
+  end
+
   private
   def employee_params
     params.require(:employee).permit(:first_name, :last_name, :email, :company_id, :area_id)
--- app/models/area.rb
+++ app/models/area.rb
@@ -1,3 +1,4 @@
 class Area < ApplicationRecord
   has_many :employees
+  has_many :companies, :through => :employees
 end
